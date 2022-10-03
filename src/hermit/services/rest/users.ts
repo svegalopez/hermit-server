626eeddffa78c71749ad43db9c14d265f874c752
Sebastian Vega <svegalopez@gmail.com>
2022-10-03 16:45:08 -0400

    updates

--- src/hermit/services/rest/users.ts
+++ src/hermit/services/rest/users.ts
@@ -1,36 +1,23 @@
 import express, { RequestHandler } from 'express';
 import { compareSync, hashSync } from 'bcrypt';
 import { randomUUID } from 'crypto';
-const jwt = require('jsonwebtoken');
+import { User } from '@prisma/client';
+import { JwtPayload, verify, sign } from 'jsonwebtoken';
 
 const secret = '123456789'; // Dummy secret, use env variable
 const router = express.Router();
 
 export const authenticate: RequestHandler = async (req, res, next) => {
     const token = req.header('Authorization');
-    const agentKey = req.cookies['Agent-Key'];
+    if (!token) return res.status(400).send({ error: 'Please provide your credentials' });
 
-    if (!token) return res.status(400).send({ error: 'No token' });
-    if (!agentKey) return res.status(400).send({ error: 'No key' });
-
-    jwt.verify(token, secret, async (err: Error, decoded: { id: number }) => {
-        if (err) return res.status(401).send({ error: "No bueno compadre!" });
+    verify(token, secret, async (err, decoded) => {
+        if (err) return res.status(401).send({ error: "Unable to verify credentials" });
 
         const user = await req.prisma.user.findUnique({
-            where: { id: decoded.id },
-            select: {
-                email: true,
-                id: true,
-                logins: {
-                    where: {
-                        userId: decoded.id,
-                        agentKey
-                    }
-                }
-            }
+            where: { id: (decoded as JwtPayload).id },
         })
-        if (!user) return res.status(404).send({ error: "No bueno compadre!" });
-        if (!user.logins[0]) return res.status(404).send({ error: "No logins found" });
+        if (!user) return res.status(404).send({ error: "User not found" });
 
         req.user = user;
         next();
@@ -64,41 +51,63 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
     const user = await req.prisma.user.findUnique({ where: { email } })
-    if (!user) return res.sendStatus(400);
+    if (!user) return res.status(404).send({ error: "User not found" });
 
     if (compareSync(password, user.password)) {
-        const token = jwt.sign({ id: user.id }, secret);
 
+        const token = sign({ id: user.id }, secret);
         const agentKey = randomUUID();
         await req.prisma.userLogin.create({
             data: {
                 agentKey,
                 userId: user.id
             }
-        })
+        });
+
+        delete (user as Partial<User>).password;
         return res.cookie('Agent-Key', agentKey, {
             httpOnly: true
-        }).json({ token });
+        }).json({ token, user });
+
     } else {
-        return res.sendStatus(401);
+        return res.status(401).send({ error: 'Incorrect Password. Your account will be blocked after 2 more failed attempts.' });
     }
 });
 
-router.delete('/logout', authenticate, async (req, res) => {
-    await req.prisma.userLogin.delete({
+router.delete('/logout', async (req, res) => {
+    const agentKey = req.cookies['Agent-Key'];
+    if (!agentKey) return res.sendStatus(404);
+
+    const userLogin = await req.prisma.userLogin.delete({
         where: {
-            userId_agentKey: {
-                userId: req.user!.id,
-                agentKey: req.cookies['Agent-Key']
+            agentKey
+        }
+    }).catch((err: Error) => console.error(err))
+
+    if (userLogin) return res.sendStatus(200);
+    return res.sendStatus(404);
+});
+
+router.get('/current', async (req, res) => {
+    const agentKey = req.cookies['Agent-Key'];
+    if (!agentKey) return res.status(400).send({ err: 'Missing credentials' })
+
+    const login = await req.prisma.userLogin.findFirst({
+        where: { agentKey },
+        select: {
+            user: {
+                select: {
+                    id: true,
+                    email: true
+                }
             }
         }
     })
-    res.sendStatus(200);
-});
 
-router.get('/current', authenticate, async (req, res) => {
-    console.log(req.header('Cookie'));
-    return res.json(req.user);
+    if (!login) return res.status(401).send({ err: 'Invalid credentials' })
+    const token = sign({ id: login.user.id }, secret);
+
+    return res.json({ user: login.user, token });
 });
 
 export default router;
