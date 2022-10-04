7002c6df3661bef05a0e29557d5c201cb3b4f890
Sebastian Vega <svegalopez@gmail.com>
2022-10-04 15:02:04 -0400

    ready to deploy

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import listen from './hermit';
 
 // This program relies on the "DATABASE_URL" env variable 
 async function main() {
-    await listen(4000);
+    await listen(process.env.PORT || 4000);
 }
 
 main();
