c6ea865cbc44ca76df5dafc62dedde1346873b98
Sebastian Vega <svegalopez@gmail.com>
2022-10-06 13:10:35 -0400

    push to main

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import listen from './hermit';
 
-// This program relies on the "DATABASE_URL" env variable 
+// This program relies on the "DATABASE_URL" env variable.
 async function main() {
     await listen(process.env.PORT || 4000);
 }
